737e8893e6572e1209c83e582a88fb5ea99535bf
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-12-02 12:24:30 -0500

    Cambios

---
+++ config.js
@@ -0,0 +1,18 @@
+//Variable that stores the config
+var config = {};
+
+//Email account on Mockaroo with Schemas
+config.username = "camilon9035@hotmail.com";
+//Password of Mockarooo (Dont panic, you can look at the code, i not stealing your info)
+config.password = "123456789";
+//Times the code would be executed be aware that this would demand a bit of computer horse power
+config.timesExecuted = 30;
+//Schemas Links
+config.schemas= [
+    "https://www.mockaroo.com/schemas/142869",
+    "https://www.mockaroo.com/schemas/142918",
+    "https://www.mockaroo.com/schemas/142923",
+];
+
+//DONT TOUCH THIS :P
+module.exports= config;
---
+++ index.js
@@ -0,0 +1,55 @@
+const CONFIG = require("./config");
+const puppeteer = require('puppeteer');
+const { Cluster } = require('puppeteer-cluster');
+const fs = require("fs")
+
+
+
+
+const scrape = async () => {
+    // Create a cluster with 2 workers
+    const cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 2,
+        monitor:true,
+
+    });
+
+    // Define a task (in this case: screenshot of page)
+    await cluster.task(async ({ page, data: url }) => {
+        await page.goto("https://www.mockaroo.com/users/sign_in", {waitUntil:"networkidle0"});
+        await page.type("#user_email",CONFIG.username);
+        await page.type("#user_password",CONFIG.password);
+        await page.click("input.btn.btn-success")
+        await page.waitFor(1000);
+        await page.goto(url);
+        const title = await page.evaluate(()=>{
+            return document.querySelector("H2").innerHTML;  
+        })
+        console.log(title);
+        const pathData ="./data/"+title+"/"+ Date.now();
+        
+        await page._client.send('Page.setDownloadBehavior', {
+            behavior: 'allow',
+            downloadPath: pathData
+            });
+        await page.click("#download");
+        await page.waitFor(10000);
+    });
+
+    // Add some pages to queue
+    for(page of CONFIG.schemas){
+        await cluster.queue(page);
+    }
+
+    // Shutdown after everything is done
+    await cluster.idle();
+    await cluster.close();
+}
+
+
+let cont = 0;
+while(cont<CONFIG.timesExecuted){
+    await scrape();
+    cont++
+}
